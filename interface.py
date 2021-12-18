df55b426d83d015b10dacbd0c21c09972fb692ed
apfdamascena <apfd@cin.ufpe.br>
2021-12-18 11:32:08 -0300

    feature(base): interface project

---
+++ interface.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+class GUI:
+    def __init__(self, width, height):
+        self.window = Tk()
+        self.window.title('4 Atividade')
+
+        self.canva = Canvas(self.window, width, height)
+        self.canva.grid(columnspan=3)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.txt_area = Text(self.canva, border=1)
+        self.txt_field = Entry(self.canva, width=85, border=1, bg='white')
+        self.send_button = Button(self.canva, text='Send', padx=40, command=self.send)
+
+        self.txt_area.config(background='#abd3eb')
+
+        self.txt_area.grid(column=0, row=0, columnspan=3)
+        self.txt_field.grid(column=0, row=1, columnspan=2)
+        self.send_button.grid(column=2, row=1)
+
+    def send(self, event=None):
+        text = self.txt_field.get() + '\n'
+        self.txt_area.insert(END, text)
+        self.txt_field.delete(0, END)
+
+    def start(self):
+        self.window.mainloop()
+        
+
+if __name__ == '__main__':
+    interface = GUI(600, 800).start()
