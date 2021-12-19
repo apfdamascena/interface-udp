9b50e758380a4ad679d74f68808be79eb9112e79
Otávio Augusto <otavioaugustomachadodeoliveira@gmail.com>
2021-12-19 13:21:11 -0300

    adicionando reprodutor de musica

--- interface.py
+++ interface.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import filedialog
 from datetime import datetime
 from PIL import Image, ImageTk
-
+import pygame
+from tkvideo import tkvideo
 
 class GUI:
     def __init__(self, width, height):
@@ -21,7 +22,7 @@ class GUI:
 
     def createWidgets(self):
         self.txt_area = Text(self.canva, border=1)
-
+        self.audio_button = Button(self.canva, text='Play Last Audio', padx=40, command=self.play_audio)
         self.txt_field = Entry(self.canva, width=85, border=1, bg='white')
         self.send_button = Button(self.canva, text='Send', padx=40, command=self.send)
         self.attachment = Button(self.canva, text='Attachment', command=self.open_dialog_with_files)
@@ -30,24 +31,50 @@ class GUI:
         self.txt_area.config(background='#abd3eb')
         
         self.txt_area.grid(column=0, row=0, columnspan=3)
-        
+        self.audio_button.grid(column=5,row=1)
         self.txt_field.grid(column=0, row=1, columnspan=2)
         self.send_button.grid(column=2, row=1)
         self.clear_button.grid(column=3, row=1)
         self.attachment.grid(column=4, row=1)
 
+        
+        
     def open_dialog_with_files(self):
         filename = filedialog.askopenfilename()
-        file_pic = Image.open(filename)
-        miniature_pic = file_pic.resize((150, (150*file_pic.height)//file_pic.width), Image.ANTIALIAS)
+        if filename[-3:] == 'jpg' or filename[-4:] == 'jpeg' or filename[-3:] == 'png': # caso seja uma imagem jpg ou jpeg vai exibir elas
+            file_pic = Image.open(filename)
+            miniature_pic = file_pic.resize((150, (150*file_pic.height)//file_pic.width), Image.ANTIALIAS)
+            
+            my_img = ImageTk.PhotoImage(miniature_pic)
+            
+            my_img.image = my_img #cria uma referência, evita que o coletor de lixo do python dê problema.
+            self.txt_area.image_create(END, image=my_img)
+            self.txt_area.insert(END,f'\n') #serve apenas pra próxima imagem não bugar e aparecer na lateral 
         
-        my_img = ImageTk.PhotoImage(miniature_pic)
+        elif filename[-3:] == "wav" or filename[-3:] == 'mp3': #reproduz música
+            
+            #exibe o ícone do mp3
+            file_pic = Image.open('mp3.png')
+            miniature_pic = file_pic.resize((120, (120*file_pic.height)//file_pic.width), Image.ANTIALIAS)
+            my_img = ImageTk.PhotoImage(miniature_pic)
+            my_img.image = my_img 
+            self.txt_area.image_create(END, image=my_img)
+            self.txt_area.insert(END,f'\n')
+            
+            self.last_audio = filename 
+            
+            
+        '''elif filename[-3:] == 'mp4':
+            player = tkvideo(filename, self.window ,loop = 1, size = (700, 500))
+            player.play()'''
+    
+    
+    
+    def play_audio(self):
+            pygame.init()
+            pygame.mixer.music.load(self.last_audio)
+            pygame.mixer.music.play(loops=0)
         
-        
-        my_img.image = my_img #cria uma referência, evita que o coletor de lixo do python dê problema.
-        
-        self.txt_area.image_create(END, image=my_img)
-        self.txt_area.insert(END,f'\n') #serve apenas pra próxima imagem não bugar e aparecer na lateral
         
         
     
