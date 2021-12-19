2cfc31c6de99ceaf3b7044799cefd4ee28fd10ff
Otávio Augusto <otavioaugustomachadodeoliveira@gmail.com>
2021-12-19 00:26:27 -0300

    exibir imagens na tela

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from datetime import datetime
+from PIL import Image, ImageTk
 
 
 class GUI:
@@ -10,6 +11,7 @@ class GUI:
 
         self.canva = Canvas(self.window, width=width, height=height)
         self.canva.grid(columnspan=3)
+        #self.window.geometry(f"{width}x{height}")
         
         self.createWidgets()
         self.__bind()
@@ -19,14 +21,16 @@ class GUI:
 
     def createWidgets(self):
         self.txt_area = Text(self.canva, border=1)
+
         self.txt_field = Entry(self.canva, width=85, border=1, bg='white')
         self.send_button = Button(self.canva, text='Send', padx=40, command=self.send)
         self.attachment = Button(self.canva, text='Attachment', command=self.open_dialog_with_files)
         self.clear_button = Button(self.canva, text='Clear', padx=40, command=self.clear)
-        
-        self.txt_area.config(background='#abd3eb')
 
+        self.txt_area.config(background='#abd3eb')
+        
         self.txt_area.grid(column=0, row=0, columnspan=3)
+        
         self.txt_field.grid(column=0, row=1, columnspan=2)
         self.send_button.grid(column=2, row=1)
         self.clear_button.grid(column=3, row=1)
@@ -34,8 +38,19 @@ class GUI:
 
     def open_dialog_with_files(self):
         filename = filedialog.askopenfilename()
-        my_image = PhotoImage(file=filename)
-        self.txt_area.image_create(END, image = my_image)
+        file_pic = Image.open(filename)
+        miniature_pic = file_pic.resize((150, (150*file_pic.height)//file_pic.width), Image.ANTIALIAS)
+        
+        my_img = ImageTk.PhotoImage(miniature_pic)
+        
+        
+        my_img.image = my_img #cria uma referência, evita que o coletor de lixo do python dê problema.
+        
+        self.txt_area.image_create(END, image=my_img)
+        self.txt_area.insert(END,f'\n') #serve apenas pra próxima imagem não bugar e aparecer na lateral
+        
+        
+    
 
     def send(self, event=None):
         text = self.check_valid_input(self.txt_field.get())
@@ -57,4 +72,4 @@ class GUI:
         
 
 if __name__ == '__main__':
-    interface = GUI(600, 800).start()
+    interface = GUI(800, 600).start()
