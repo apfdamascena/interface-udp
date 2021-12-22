eafd43fb448b073038e57dbb7752859c4cf9e6c2
CaiolBarreto <caiobarreto_28@hotmail.com>
2021-12-21 22:04:21 -0300

    feature(connect-client): solved some bugs

--- MessageCheck.py
+++ MessageCheck.py
@@ -5,9 +5,8 @@ class MessageCheck:
     def __init__(self):
         self.__non_printable = ['Waiting another user', 'Invalid message! Please, try again']
 
-
     def is_printable(self, message):
-        return not ((message in self.__non_printable) or (message[0] == '(' and message[-1] == ')'))
+        return not ((message.strip() in self.__non_printable) or (message[0] == '(' and message[-1] == ')'))
 
     def is_valid_input(self, from_user_input):
         if not from_user_input.strip():
--- interface.py
+++ interface.py
@@ -6,6 +6,7 @@ from ExtensionType import ExtensionType
 from MessageCheck import MessageCheck
 from TypeFile import TypeFile
 import os 
+import sys
 
 class GUI:
     def __init__(self, width, height):
@@ -24,7 +25,7 @@ class GUI:
         self.__client.listenning()
 
         self.__video_counter = 0
-        self.__audio_conter  = 0
+        self.__audio_counter = 0
 
     def __createWidgets(self):
         self.__txt_area = Text(self.__canva, border=1)
@@ -34,8 +35,8 @@ class GUI:
        
         self.__attachment = Button(self.__canva, text='Attachment', command=self.__open_dialog_with_files)
         self.__send_button = Button(self.__canva, text='Send', padx=40, command=self.__send)
-        self.__audio_button = Button(self.__canva, text='Play Audio', padx=40, command=self.__play_audio)
         self.__video_button = Button(self.__canva, text='Play Video', padx=40, command=self.__play_video)
+        self.__audio_button = Button(self.__canva, text='Play Audio', padx=40, command=self.__play_audio)
         self.__clear_button = Button(self.__canva, text='Clear', padx=40, command=self.__clear)
 
         self.__listaudio = Listbox(selectmode = SINGLE, width = 30)
@@ -46,12 +47,22 @@ class GUI:
         self.__send_button.grid(column=2, row=1)
         self.__clear_button.grid(column=3, row=1)
         self.__attachment.grid(column=4, row=1)
-        self.__audio_button.grid(column=5,row=1)
-        self.__video_button.grid(column=6, row=1)
+        self.__video_button.grid(column=5, row=1)
+        self.__audio_button.grid(column=6,row=1)
         self.__txt_area.grid(column=0, row=0, columnspan=3)
         self.__txt_field.grid(column=0, row=1, columnspan=2)
         
         self.__txt_area.config(background='#abd3eb')
+
+    def __get_open_command(self, filename):
+        operational_system = sys.platform
+        if operational_system in ['linux', 'linux2']:
+            filename = filename.replace(' ', '\ ')
+            return f'xdg-open {filename}'
+        elif operational_system in ['win32', 'win64']:
+            return f'"{filename}"'
+        else:
+            return f'open {filename}'
 
     def __open_dialog_with_files(self):
         filename = filedialog.askopenfilename()
@@ -60,11 +71,10 @@ class GUI:
         self.__image_on_screen.add(extension_type, filename)
 
         if extension_type == TypeFile.AUDIO:
-           self.__last_audio = filename
-           self.__add_list_audio()
+           self.__add_list_audio(filename)
 
         if extension_type == TypeFile.VIDEO:
-           self.__add_list_video()
+           self.__add_list_video(filename)
 
         self.__send_file(filename)
          
@@ -82,11 +92,11 @@ class GUI:
 
     def __play_video(self):
         chosen_video = self.__listvideo.get(self.__listvideo.curselection())
-        os.system(f"open {chosen_video}")
+        os.system(self.__get_open_command(chosen_video))
 
     def __play_audio(self):
-        chosen_video = self.__listaudio.get(self.__listaudio.curselection())
-        os.system(f"open {chosen_video}")
+        chosen_audio= self.__listaudio.get(self.__listaudio.curselection())
+        os.system(self.__get_open_command(chosen_audio))
     
     def receive_and_show_at_screen(self, message):
         if self.__message_checker.is_printable(message): 
@@ -98,10 +108,10 @@ class GUI:
         self.__image_on_screen.add(extension_type, filename)
 
         if extension_type == TypeFile.AUDIO:
-            self.__add_list_audio()
+            self.__add_list_audio(filename)
 
         if extension_type == TypeFile.VIDEO:
-            self.__add_list_video()
+            self.__add_list_video(filename)
 
     def __send(self, event=None):
         text = self.__message_checker.is_valid_input(self.__txt_field.get())
@@ -115,16 +125,15 @@ class GUI:
     def start(self):
         self.__window.mainloop()
 
-    def __add_list_audio(self):
-        self.__listaudio.insert(self.__audio_conter ,filename)
-        self.__audio_conter += 1
+    def __add_list_audio(self, filename):
+        self.__listaudio.insert(self.__audio_counter, filename)
+        self.__audio_counter += 1
 
-    def __add_list_video(self):
-        self.__listvideo.insert(self.__video_counter,filename)
+    def __add_list_video(self, filename):
+        self.__listvideo.insert(self.__video_counter, filename)
         self.__video_counter += 1
 
-
-        
+    
 
 if __name__ == '__main__':
     interface = GUI(800, 600).start()
